06b1b031c31338e420ba74142f6048417aa44300
aroob awan <aroobawan64@gmail.com>
2026-04-20 14:01:00 -0700

    changed api keys

--- Project/Part-2/team-1/admin.py
+++ Project/Part-2/team-1/admin.py
@@ -1,9 +1,9 @@
 import psycopg2
 
 DATABASE_URL = (
-    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-    "neondb?sslmode=require"
+"postgresql://readonly:ReadOnly456%21@"
+"ep-curly-bird-anqf0rre-pooler.c-6.us-east-1.aws.neon.tech/"
+"neondb?sslmode=require&channel_binding=require"
 )
 
 def _get_conn():
