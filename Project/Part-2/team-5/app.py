8b39aaa101cf0824c0da8f0b392177435acde77d
NupurSompura <n_sompura@u.pacific.edu>
2026-04-20 16:27:43 -0700

    fixed slow and fast query

--- Project/Part-2/team-5/app.py
+++ Project/Part-2/team-5/app.py
@@ -1,4 +1,5 @@
 import psycopg2
+import time
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -129,55 +130,50 @@ def water_flower(id):
 
 @app.route('/flowers/slow_query', methods=['GET'])
 def slow_query():
+    start = time.perf_counter()
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("""
-                SELECT COUNT(*) FROM (
-                    SELECT f.id, f.name, f.last_watered, f.water_level, f.min_water_required,
-                        c.name AS customer_name, o.order_date
-                    FROM team5_flowers f
-                    CROSS JOIN (SELECT * FROM team5_orders
-                    CROSS JOIN team5_customers)
-                    WHERE name LIKE '_e' AND water_level < min_water_required
-                    ORDER BY o.order_date DESC
-                );
+                SELECT f.id, f.name, f.last_watered, f.water_level, f.min_water_required,
+                    c.name AS customer_name, o.order_date,
+                    pgp_sym_encrypt(c.email::text, 'encrypt_key') AS encrypted_email
+                FROM team5_flowers f
+                CROSS JOIN team5_orders o
+                CROSS JOIN team5_customers c
+                WHERE LOWER(f.name) LIKE '%e'
+                ORDER BY UPPER(c.name) DESC;
     """)
     results = cur.fetchall()
     cur.close()
     conn.close()
-
-    return jsonify([{
-        "id": r[0], "name": r[1], "last_watered": r[2].strftime("%Y-%m-%d"),
-        "water_level": r[3], "min_water_required": r[4],
-        "customer_name": r[5], "order_date": r[6].strftime("%Y-%m-%d") if r[6] else None
-    } for r in results])
-
+    end = time.perf_counter()
+    elapsed_time  = (end - start)
+    return jsonify({
+        "execution_time": elapsed_time
+    })
 @app.route('/flowers/fast_query', methods=['GET'])
 def fast_query():
+    start = time.perf_counter()
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("""
         SELECT f.id, f.name, f.last_watered, f.water_level, f.min_water_required,
-               c.name AS customer_name, o.order_date
+            c.name AS customer_name, o.order_date
         FROM team5_flowers f
-        JOIN team5_orders o ON f.id = o.flower_id
-        JOIN team5_customers c ON o.customer_id = c.id
-        WHERE f.name LIKE 'Rose%'
-          AND c.name LIKE 'Customer%'
-        ORDER BY o.order_date DESC
-        LIMIT 100 OFFSET 0
+        INNER JOIN team5_orders o ON f.id = o.flower_id
+        INNER JOIN team5_customers c ON o.customer_id = c.id
+        WHERE f.name LIKE 'E%'
+        ORDER BY c.name DESC
+        LIMIT 100;
     """)
-
     results = cur.fetchall()
     cur.close()
     conn.close()
-
-    return jsonify([{
-        "id": r[0], "name": r[1], "last_watered": r[2].strftime("%Y-%m-%d"),
-        "water_level": r[3], "min_water_required": r[4],
-        "customer_name": r[5], "order_date": r[6].strftime("%Y-%m-%d") if r[6] else None
-    } for r in results])
-
+    end = time.perf_counter()
+    elapsed_time  = (end - start)
+    return jsonify({
+        "execution_time": elapsed_time
+    })
 # Use this algorithm provided
     # UPDATE teamX_flowers
     # SET water_level = water_level - (5 * (CURRENT_DATE - last_watered));
