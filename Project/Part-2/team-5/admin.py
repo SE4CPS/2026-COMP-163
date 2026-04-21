d477300b908b14f91a8a1e4a1c0656337b7505b1
NupurSompura <n_sompura@u.pacific.edu>
2026-04-20 17:26:05 -0700

    changed the database url

--- Project/Part-2/team-5/admin.py
+++ Project/Part-2/team-5/admin.py
@@ -1,9 +1,10 @@
 import psycopg2
 
 DATABASE_URL = (
-    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-    "neondb?sslmode=require"
+    "postgresql://neondb_owner:npg_ZXbPjA2tOf6K@ep-green-pond-amrjk7ka-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    # "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
+    # "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
+    # "neondb?sslmode=require"
 )
 
 def _get_conn():
--- Project/Part-2/team-5/app.py
+++ Project/Part-2/team-5/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 # Database connection details ADDED
 DATABASE_URL = (
-    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-    "neondb?sslmode=require"
+    "postgresql://neondb_owner:npg_ZXbPjA2tOf6K@ep-green-pond-amrjk7ka-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    # "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
+    # "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
+    # "neondb?sslmode=require"
 )
 
 def get_db_connection():
