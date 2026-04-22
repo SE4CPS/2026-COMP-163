165a53a84b10d2342e8142fa427d781ddb272b1a
Fabian Beltran <fabianbrine@gmail.com>
2026-04-22 07:45:08 -0700

    updated db url to new neon url

--- Project/Part-2/team-9/db_conn.py
+++ Project/Part-2/team-9/db_conn.py
@@ -1,15 +1,5 @@
-
-# course neondb
-#DATABASE_URL = (
-#    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-#    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-#    "neondb?sslmode=require"
-#)
-
-# local db because it hit the usage limit
+# fabians neondb
 DATABASE_URL = (
-    "postgresql://postgres@"
-    "localhost:5432/"
-    ""
+   "postgresql://neondb_owner:npg_whm15JkFdtXS@ep-green-boat-amxshjwq-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 )
 
