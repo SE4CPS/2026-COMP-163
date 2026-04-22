d60ca9531b67bd038289203deca96414b55dfa12
AnhP <112260892+AnhP1205@users.noreply.github.com>
2026-04-21 19:32:56 -0700

    New Neon API

--- Project/Part-2/team-3/backend.py
+++ Project/Part-2/team-3/backend.py
@@ -1,10 +1,9 @@
 import psycopg2
 from datetime import date
-
 DATABASE_URL = (
-    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-    "neondb?sslmode=require"
+    "postgresql://neondb_owner:npg_mCN6deqDx1iO@"
+    "ep-square-lab-ambs03t1-pooler.c-5.us-east-1.aws.neon.tech/"
+    "neondb?sslmode=require&channel_binding=require"
 )
 
 def _get_conn():
