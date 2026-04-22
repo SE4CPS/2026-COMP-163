da4120fccde2e1bb23762563db8d6b2bf7650f07
Nathan <85586026+nathlinear@users.noreply.github.com>
2026-04-21 22:21:04 -0700

    Add slow query

--- Project/Part-1/team-10/backend.py
+++ Project/Part-1/team-10/backend.py
@@ -1,9 +1,9 @@
 import psycopg2
 
 DATABASE_URL = (
-    "postgresql://neondb_owner:npg_M5sVheSzQLv4@"
-    "ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/"
-    "neondb?sslmode=require"
+    "postgresql://neondb_owner:npg_b64dzjqCkBiF@"
+    "ep-soft-king-anuhub9k-pooler.c-6.us-east-1.aws.neon.tech/"
+    "neondb?sslmode=require&channel_binding=require"
 )
 
 def _get_conn():
@@ -104,6 +104,25 @@ def water_flower(id, amount):
         conn.rollback()
         print("Water error:", e)
         return False
+    finally:
+        cur.close()
+        conn.close()
+
+def slow():
+    conn = _get_conn()
+    cur = conn.cursor()
+    try:
+        cur.execute("""
+            EXPLAIN ANALYZE
+            SELECT * FROM team10_flowers f
+                FULL JOIN team10_orders ON 1=1
+                FULL JOIN team10_customers ON 1=1
+            WHERE f.name LIKE '%'
+            ORDER BY RANDOM
+            ;
+        """)
+        rows = cur.fetchall()
+        return rows
     finally:
         cur.close()
         conn.close()
